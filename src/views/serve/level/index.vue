169c9085c05530e07ce45935c360a14a74cca536
AdrianDeGit <adriandeemail@gmail.com>
2025-07-12 20:37:43 +0800

    feat: 实现护理等级页面布局

--- src/views/serve/level/index.vue
+++ src/views/serve/level/index.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="等级名称" prop="name">
+      <el-form-item label="名称" prop="name">
         <el-input
           v-model="queryParams.name"
-          placeholder="请输入等级名称"
+          placeholder="请输入护理等级名称"
           clearable
           @keyup.enter="handleQuery"
         />
+      </el-form-item>
+      <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择" clearable>
+          <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"/>
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -15,7 +20,7 @@
       </el-form-item>
     </el-form>
 
-    <el-row :gutter="10" class="mb8">
+    <el-row :gutter="10" class="mb8" type="flex" justify="end">
       <el-col :span="1.5">
         <el-button
           type="primary"
@@ -23,47 +28,20 @@
           icon="Plus"
           @click="handleAdd"
           v-hasPermi="['serve:level:add']"
-        >新增</el-button>
+        >新增护理等级</el-button>
       </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['serve:level:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['serve:level:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['serve:level:export']"
-        >导出</el-button>
-      </el-col>
-      <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="levelList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="编号" align="center" prop="id" />
-      <el-table-column label="等级名称" align="center" prop="name" />
-      <el-table-column label="护理计划ID" align="center" prop="lplanId" />
-      <el-table-column label="护理费用" align="center" prop="fee" />
-      <el-table-column label="状态" align="center" prop="status" />
+      <el-table-column label="序号" align="center" prop="id" />
+      <el-table-column label="护理等级名称" align="center" prop="name" />
+      <el-table-column label="执行护理计划" align="center" prop="lplanId" />
+      <el-table-column label="护理费用(元/月)" align="center" prop="fee" />
+      <el-table-column label="状态" align="center" prop="status" >
+        <template #default="scope">
+          <el-tag :type="scope.row.status === 1 ? 'success' : 'danger'">{{ scope.row.status === 1 ? '启用' : '禁用' }}</el-tag>
+        </template>
+      </el-table-column>
       <el-table-column label="等级说明" align="center" prop="description" />
       <el-table-column label="创建时间" align="center" prop="createTime" width="180">
         <template #default="scope">
@@ -74,6 +52,7 @@
         <template #default="scope">
           <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['serve:level:edit']">修改</el-button>
           <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['serve:level:remove']">删除</el-button>
+          <el-button link type="primary" :icon="scope.row.status === 0 ? 'Lock' : 'Unlock'" @click="handleEnable(scope.row)" >{{ scope.row.status === 0 ? '启用' : '禁用' }}</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -239,7 +218,7 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value
-  proxy.$modal.confirm('是否确认删除护理等级编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除护理等级序号为"' + _ids + '"的数据项？').then(function() {
     return delLevel(_ids)
   }).then(() => {
     getList()
@@ -255,4 +234,37 @@ function handleExport() {
 }
 
 getList()
+
+//下拉选择框的选择项
+const options = ref([
+  {
+    value: 1,
+    label: "启用"
+  },
+  {
+    value: 0,
+    label: "停用"
+  }
+])
+
+/* 启用禁用按钮操作 */
+function handleEnable(row) {
+  // 获取当前状态
+  const status = row.status;
+  // 提示信息
+  const msg = status === 1 ? '禁用' : '启用';
+
+  // 构建参数
+  const params = {
+    id: row.id,
+    status: status === 1 ? 0 : 1
+  };
+
+  proxy.$modal.confirm(`是否确认${msg}该护理等级的数据项？`).then(function() {
+    return updateLevel(params);
+  }).then(() => {
+    getList();
+    proxy.$modal.msgSuccess(`${msg}成功`);
+  }).catch(() => {});
+}
 </script>
