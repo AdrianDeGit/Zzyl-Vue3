71f6d3ca55edbe1e3fdc14774a65f4cdd4a56937
AdrianDeGit <adriandeemail@gmail.com>
2025-07-13 10:47:04 +0800

    feat(plan.js): 新增修改护理计划状态接口

--- src/api/serve/plan.js
+++ src/api/serve/plan.js
@@ -2,43 +2,52 @@ import request from '@/utils/request'
 
 // 查询护理计划列表
 export function listPlan(query) {
-  return request({
-    url: '/serve/plan/list',
-    method: 'get',
-    params: query
-  })
+    return request({
+        url: '/serve/plan/list',
+        method: 'get',
+        params: query
+    })
 }
 
 // 查询护理计划详细
 export function getPlan(id) {
-  return request({
-    url: '/serve/plan/' + id,
-    method: 'get'
-  })
+    return request({
+        url: '/serve/plan/' + id,
+        method: 'get'
+    })
 }
 
 // 新增护理计划
 export function addPlan(data) {
-  return request({
-    url: '/serve/plan',
-    method: 'post',
-    data: data
-  })
+    return request({
+        url: '/serve/plan',
+        method: 'post',
+        data: data
+    })
 }
 
 // 修改护理计划
 export function updatePlan(data) {
-  return request({
-    url: '/serve/plan',
-    method: 'put',
-    data: data
-  })
+    return request({
+        url: '/serve/plan',
+        method: 'put',
+        data: data
+    })
+}
+
+// 修改护理计划状态
+export function planStatus(data) {
+    return request({
+        url: '/serve/plan',
+        method: 'put',
+        data: data
+    })
 }
 
 // 删除护理计划
 export function delPlan(id) {
-  return request({
-    url: '/serve/plan/' + id,
-    method: 'delete'
-  })
+    return request({
+        url: '/serve/plan/' + id,
+        method: 'delete'
+    })
 }
